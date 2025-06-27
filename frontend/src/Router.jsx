63b91dfcf6367a999f7b373625453b0b9e61d248
Pushpaja Priyadarshini <pushpajapriyadarshini@gmail.com>
2025-06-27 14:00:11 +0530

    Updated Router component

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Registration from "./components/Registration";
+import Login from "./components/Login";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Registration/>} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };

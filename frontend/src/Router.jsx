8a0834360760d15a5eb4a2b2dc01cfefc76a6c76
Pushpaja Priyadarshini <pushpajapriyadarshini@gmail.com>
2025-06-27 16:07:02 +0530

    Modify new route

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Registration from "./components/Registration";
 import Login from "./components/Login";
+import VolunteerSignup from "./components/VolunteerSignup";
+
 
 const AppRoutes = () => {
   return (
@@ -10,6 +12,10 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Registration/>} />
       <Route path="/login" element={<Login />} />
+     
+      <Route path="/volunteer-signup" element={<VolunteerSignup />} />
+
+      
     </Routes>
   );
 };

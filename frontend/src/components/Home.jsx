440d99ae5006c3eb22755ea56d7dd0e52f85dacc
Pushpaja Priyadarshini <pushpajapriyadarshini@gmail.com>
2025-06-27 16:05:37 +0530

    Add Volunteer Sign Up Button

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Link
+  to="/volunteer-signup"
+  className="inline-block bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-500 hover:to-purple-500 text-white font-bold py-2.5 px-6 rounded-xl text-sm md:text-base mt-4"
+>Volunteer-Signup Page</Link>
     </div>
   )
 }
